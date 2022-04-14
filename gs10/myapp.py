52e8335da782c8e959e7b421e8ba0303ea647d68
sanya midha <sanyamidha.10@gmail.com>
2022-04-14 15:22:05 +0530

    Function based api_view using third party application

---
+++ gs10/myapp.py
@@ -0,0 +1,65 @@
+import requests
+import json
+
+URL = 'http://127.0.0.1:8000/studentapi/'
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id':id}
+
+    json_data = json.dumps(data)
+    headers = {'content-Type':'application/json'}
+    r = requests.get(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print (data)
+
+# get_data()
+
+def post_data():
+    data = {
+        'name':'Rani',
+        'roll':107,
+        'city':'Bokaro',
+    }
+
+    json_data = json.dumps(data)
+    headers = {'content-Type':'application/json'}
+    r = requests.post(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+
+# post_data()
+
+
+def update_data():
+    data = {
+        'id':4,
+        'name':'SanyaMidha',
+        'city':'Chandigarh',
+    }
+
+    json_data = json.dumps(data)
+    headers = {'content-Type':'application/json'}
+    r = requests.put(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+
+# update_data()
+
+
+def delete_data():
+    data = {
+        'id':4,
+    }
+
+    json_data = json.dumps(data)
+    headers = {'content-Type':'application/json'}
+    r = requests.delete(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+
+delete_data()
+
+
+
