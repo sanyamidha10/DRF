32600e6e0fb46ce9c8a53b58f70875b0c1e4eb8c
sanya midha <sanyamidha.10@gmail.com>
2022-04-15 17:02:47 +0530

    All mixins together

---
+++ gs14/api/views.py
@@ -0,0 +1,37 @@
+# GenericAPIView and Model Mixins
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+
+# List and create do not require pk that is why they can be grouped together.(LC)
+class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+
+# Retrieve, update and Destroy can be grouped together because they need pk.
+class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
