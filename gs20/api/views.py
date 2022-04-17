af882a407e10453617f06da65de233a703fc3066
sanya midha <sanyamidha.10@gmail.com>
2022-04-17 15:39:06 +0530

    BasicAuthentication

---
+++ gs20/api/views.py
@@ -0,0 +1,20 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # If we have multiple classes and want to apply same authentication and permission on them , then we can also write some code inside settings.py(globally) rather than writing explicitly in each class like this below one.
+
+    authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    permission_classes = [IsAdminUser]  #This means is_staff=True
+
+
+
+    # Moreover, if you want to override in any class, then you can also do the same by writing the above commented code.
